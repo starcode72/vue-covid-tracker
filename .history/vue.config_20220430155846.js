6798a3581305c48540452fec4493ce74e1b1a3a2
starcode72 <93559665+starcode72@users.noreply.github.com>
2022-04-30 16:13:13 +0900

    dist2

--- .history/vue.config_20220430155846.js
+++ .history/vue.config_20220430155846.js
@@ -2,4 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-publicPath: process.env.NODE_ENV === "production" ? "/vue-covid-tracker/" : "/";
+publicPath: process.env.NODE_ENV === "production" ? "/my-project/" : "/";
